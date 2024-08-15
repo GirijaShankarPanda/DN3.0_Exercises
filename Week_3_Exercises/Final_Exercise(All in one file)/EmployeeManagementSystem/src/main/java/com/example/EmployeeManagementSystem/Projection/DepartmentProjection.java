07b688510f2bcd64d8a3a520cfb5047fd7118a0d
Girija Shankar Panda <104772357+GirijaShankarPanda@users.noreply.github.com>
2024-08-15 16:45:30 +0530

    Delete Week_3_Exercises/Final_Exercise(All in one file)/EmployeeManagementSystem directory

--- Week_3_Exercises/Final_Exercise(All in one file)/EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/Projection/DepartmentProjection.java
+++
@@ -1,6 +0,0 @@
-package com.example.EmployeeManagementSystem.Projection;
-
-public interface DepartmentProjection {
-    Long getId();
-    String getName();
-}
--- Week_3_Exercises/Final_Exercise(All in one file)/EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/Projection/EmployeeProjection.java
+++
@@ -1,8 +0,0 @@
-package com.example.EmployeeManagementSystem.Projection;
-
-public interface EmployeeProjection {
-    Long getId();
-    String getName();
-    String getEmail();
-    String getDepartmentName();
-}
